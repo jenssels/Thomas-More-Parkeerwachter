eda4ebc0ce11d2599e664fac9cabe88ca3fb3caf
jenssels <jenssels1998@gmail.com>
2018-12-16 14:25:49 +0100

    Merge

---
+++ app/src/main/java/be/thomasmore/tm_parkeerwachter/OvertredingDetailActivity.java
@@ -0,0 +1,13 @@
+package be.thomasmore.tm_parkeerwachter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OvertredingDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overtreding_detail);
+    }
+}
